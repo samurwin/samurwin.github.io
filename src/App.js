82050ed488c32739eb17cb5e20dda09e2577363c
smudesign <urwinsamantha@gmail.com>
2022-03-23 13:38:00 -0400

    top section

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer'
 import Home from './pages/Home';
-// import About from './pages/About';
+import About from './pages/About';
 import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
-import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -17,8 +15,7 @@ function App() {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/portfolio' component={Portfolio} />
-            <Route exact path='/resume' component={Resume} />
-            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/about' component={About} />
           </Switch>
         <Footer />
       </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,67 @@
 import React from 'react';
+import './about.css';
 
 function About() {
     return (
-        <div className='my-3'>
-            <section className='flex-row justify-space-around align-center'>
-                <div className='col-12 col-lg-7'>
-                    <h1 className='text-primary'>Hi I'm Sam!</h1>
-                    <h4 className='text-quatrinary'>Full Stack Developer & Graphic Designer</h4>
+        <div>
+            {/* ABOUT ME */}
+            <section className='slanted-left'>
+                <div className='container flex-row justify-space-around align-center'>
+                    <div className='col-lg-4 pr-5'>
+                        <img 
+                            src={require('../assets/images/about-me.jpg')} 
+                            alt='Samantha Urwin on the Street' 
+                            className='about-img'
+                        />
+                    </div>
+
+                    <div className='col-lg-8 pl-5'>
+                        <h1 className='milgran text-quatrinary'>HI I'M SAM! <span className='desc'>DESIGNER / DEVELOPER - TORONTO, ON</span></h1>
+                        <p className='text-dark pt-4'>
+                            Sed porttitor lectus nibh. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.
+                            Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Praesent sapien massa,
+                            convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat.
+                            Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur arcu erat,
+                            accumsan id imperdiet et, porttitor at sem.
+                        </p>
+                        <br></br>
+                        <p className='text-dark'>
+                            Proin eget tortor risus. Sed porttitor lectus nibh. Praesent sapien massa, convallis a pellentesque
+                            nec, egestas non nisi. Sed porttitor lectus nibh. Nulla quis lorem ut libero malesuada feugiat.
+                            Pellentesque in ipsum id orci porta dapibus.
+                        </p>
+                    </div>
                 </div>
-                <div className='col-12 col-lg-5'>
-                    <img 
-                    src={require('../assets/images/website-heroimg.png')} 
-                    alt='Samantha Urwin at a desk' 
-                    className='hero-img'
-                    />
-                </div>
+
             </section>
-            <section className='my-3 p-4 card'>
-                <h2 className='text-center text-quatrinary'>About Me</h2>
-                <p className='text-dark my-2'>
-                I would describe myself as a dualistic person. Creativity comes easily to me and so does logic and organization. 
-                I am a great collaborator, and self-motivator. I think objectively and outside the box.
-                </p>
-                <p className='text-dark my-2'>
-                I am continuing to learn and grow as a designer working as a freelance artist as well as attending the University of Toronto SCS Coding Bootcamp. 
-                I am excited to expand my skillset in web design and development while I continue my journey as a graphic designer.
-                </p>
+
+            {/* RESUME */}
+            <section className='container dark-div w-100'>
+                {/* CONTACT */}
+                <div>
+                    <h1>CONTACT ME</h1>
+                    <div>
+                        <h5><span></span> URWINSAMANTHA@GMAIL.COM</h5>
+                        <h5><span></span> RESUME</h5>
+                    </div>
+                </div>
+
+                {/* TECHNOLOGIES LIST */}
+                <div>
+                    <h1>TECHNOLOGIES</h1>
+                    <ul>
+                        <li>
+                            <span></span>
+                            ABOBE CREATIVE CLOUD
+                        </li>
+                    </ul>
+                </div>
+
+                {/* EDUCATION LIST */}
+                <div>
+                    <h1>EDUCATION</h1>
+
+                </div>
             </section>
         </div>
     )
